89e579c8eb425357758d24824a30b51ece63d140
GeorgeHsu <George00157127@gmail.com>
2025-01-09 01:18:49 +0800

    Remove not use image

--- src/views/CollageDepartmentList.vue
+++ src/views/CollageDepartmentList.vue
@@ -38,15 +38,11 @@
             首頁 > <span class="title-name">系所單位</span>
           </span>
           <div v-for="(department, index) in CollageDepartments">
-            <a :href="department.CollageDepartmentLinkUrl" target="_blank" :title="department.CollageDepartmentName">
+            <a :href="department.CollageDepartmentLinkUrl" target="_blank" :title="department.CollageDepartmentName" >
               <div class='section-title-info' :key="department.CollageDepartmentCode">
                 <span class="section-title">
                   {{ department.Name }}
                 </span>
-                <!-- <img src="img/collage_img/line-19.png"
-                  srcset="img/collage_img/line-19@2x.png 2x,
-                          img/collage_img/line-19@3x.png 3x"
-                  class="section-title-line"> -->
               </div>
               <div class='head-card-line'>
               </div>
